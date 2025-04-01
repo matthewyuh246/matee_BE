3b2542f6ecea39d662c376166274d95595f64d33
Yuhri <matthewyuh246@gmail.com>
2025-04-01 18:36:24 +0900

    domain/user

---
+++ internal/domain/user.go
@@ -0,0 +1,13 @@
+package domain
+
+import "time"
+
+type User struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Name      string    `json:"name" gorm:"type:varchar(255)"`
+	Email     string    `json:"email" gorm:"type:varchar(255);uniqueIndex"`
+	GithubID  string    `json:"github_id" gorm:"varchar(255);uniqueIndex"`
+	AvatarURL string    `json:"avatar_url" gorm:"varchar(255)"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
