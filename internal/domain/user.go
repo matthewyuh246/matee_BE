d03555ff940664dc31bbf2435e5b74dc21edb65c
Yuhri <matthewyuh246@gmail.com>
2025-04-02 22:54:40 +0900

    dto

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -11,3 +11,11 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
+
+type UserResponse struct {
+	ID        int    `json:"id"`
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	Login     string `json:"login"`
+	AvatarURL string `json:"avatar_url"`
+}
